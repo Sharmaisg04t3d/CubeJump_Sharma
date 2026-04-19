655d34f09198bd5fa31f748c3a269c568f024669
Sharma <sharmaexists@gmail.com>
2026-04-20 00:40:50 +0530

    Modify Obstacle dimensions and add jump hitbox

--- Obstacle.h
+++ Obstacle.h
@@ -10,17 +10,20 @@ public:
 	float width;
 	float height;
 	float speed;
+	float jump_hitbox_height;
 
 	Obstacle(float x,float y){
-		width = 30;
-		height = 30;
+		width = 50;
+		height = 20;
+		jump_hitbox_height = 10;
 		object_x = x;
-		object_y = y + height;
+		object_y = y + height/2;
 		speed = 3;
 	}
 
 	void drawObstace() {
 		DrawRectangle(object_x,object_y, width, height, RED);
+		DrawRectangle(object_x, object_y - height/2, width, jump_hitbox_height, GREEN);
 	}
 
 	void moveObstacle() {
